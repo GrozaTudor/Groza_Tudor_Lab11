f7f40344ff7769528b0abfa6568276f2b161d07a
Groza Tudor <tudorgroza1969@yahoo.com>
2023-12-21 20:13:41 +0200

    Add project files.

---
+++ Groza_Tudor_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Groza_Tudor_Lab11.Data;
+
+namespace Groza_Tudor_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+        Database = new ShoppingListDatabase(new RestService());
+        MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
